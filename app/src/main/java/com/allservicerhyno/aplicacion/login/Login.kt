e03cfb49db5f7e9b6b8cc75e1b505794962dbf88
webmaster03 <csi1@grupogema.com.co>
2021-11-11 12:29:27 -0500

    hagame caso

--- app/src/main/java/com/allservicerhyno/aplicacion/login/Login.kt
+++ app/src/main/java/com/allservicerhyno/aplicacion/login/Login.kt
@@ -1,15 +1,19 @@
 package com.allservicerhyno.aplicacion.login
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import com.allservicerhyno.aplicacion.R
 import com.allservicerhyno.aplicacion.authenticate.Authentication
 import com.allservicerhyno.aplicacion.authenticate.AuthenticationData
 import com.allservicerhyno.aplicacion.authenticate.POSTAuthenticate
+import com.allservicerhyno.aplicacion.R
 import com.allservicerhyno.aplicacion.dashboard.Main
 import retrofit2.Call
 import retrofit2.Callback
@@ -17,8 +21,7 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-
-class Login: AppCompatActivity() {
+class Login1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
@@ -26,26 +29,35 @@ class Login: AppCompatActivity() {
         val pass = findViewById<EditText>(R.id.Password)
         val btnLogin = findViewById<Button>(R.id.button)
         btnLogin.setOnClickListener {
+            val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+            val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+            if (isConnected) {
+                val retrofit = Retrofit.Builder()
+                    .baseUrl("https://www.allser.com.co/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+                val service = retrofit.create(
+                    POSTAuthenticate::class.java
+                )
+                val auth = Authentication(
+                    "2.0",
+                    Authentication.Params(
+                        "odoo", user.text.toString(),
+                        pass.text.toString()
+                    )
+                )
+                val repos = service.authenticate(auth)
+                login(repos, retrofit)
+            }else{
+                Log.d("Webmaster", "Conectar al SQLite")
+            }
             if (user.length() == 0) {
                 user.error = "Ingresar su correo "
             }
             if (pass.length() == 0) {
                 pass.error = "Ingresar su contraseña "
             }
-            val retrofit = Retrofit.Builder()
-                .baseUrl("https://www.allser.com.co/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-            val service = retrofit.create(POSTAuthenticate::class.java)
-            val auth = Authentication(
-                "2.0",
-                Authentication.Params(
-                    "odoo", user.text.toString(),
-                    pass.text.toString()
-                )
-            )
-            val repos = service.authenticate(auth)
-            login(repos, retrofit)
         }
     }
 
@@ -58,19 +70,19 @@ class Login: AppCompatActivity() {
                 if (response.isSuccessful) {
                     val userData = response.body()
                     if (userData!!.result != null) {
-                        val intent = Intent(this@Login, Main::class.java)
+                        val intent = Intent(this@Login1, Main::class.java)
                         startActivity(intent)
-                        Toast.makeText(this@Login, "Bienvenido!", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this@Login1, "Bienvenido!", Toast.LENGTH_SHORT).show()
                     } else {
                         Toast.makeText(
-                            this@Login,
+                            this@Login1,
                             "Usuario/Contraseña Incorrectas",
                             Toast.LENGTH_SHORT
                         ).show()
                     }
                 } else {
                     Toast.makeText(
-                        this@Login,
+                        this@Login1,
                         "Usuario/Contraseña Incorrectas",
                         Toast.LENGTH_SHORT
                     ).show()
@@ -79,7 +91,7 @@ class Login: AppCompatActivity() {
 
             override fun onFailure(call: Call<AuthenticationData?>, t: Throwable) {
                 Toast.makeText(
-                    this@Login,
+                    this@Login1,
                     "Error de autentication. Intentelo de nuevo",
                     Toast.LENGTH_SHORT
                 ).show()
@@ -87,8 +99,3 @@ class Login: AppCompatActivity() {
         })
     }
 }
-
-
-
-
-
