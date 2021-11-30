6c54e88871bdb710b17bb8c93ce671415d31c51a
WebmasterJr04 <csi2@grupogema.com.co>
2021-11-30 10:21:41 -0500

    pruebas

--- app/src/main/java/com/allservicerhyno/aplicacion/room/Persona.kt
+++ app/src/main/java/com/allservicerhyno/aplicacion/room/Persona.kt
@@ -1,12 +1,21 @@
 package com.allservicerhyno.aplicacion.room
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "personas")
 data class Persona(
-   @PrimaryKey(autoGenerate = true)
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
     val id: Long= 0,
+    @ColumnInfo(name = "Login")
     val email: String,
+    @ColumnInfo(name = "Password")
     val password: String
 )
+{
+    override fun toString(): String {
+        return "Persona(id=$id, email='$email', password='$password')\n"
+    }
+}
